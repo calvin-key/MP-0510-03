b89e3731fe494320fc67c724904ef0afac1c8388
zorifudin <zorifimaduddin@gmail.com>
2024-12-14 19:45:17 +0700

    feat:add forgot password

--- src/config.ts
+++ src/config.ts
@@ -10,3 +10,7 @@ export const SUPABASE_DATABASE = process.env.SUPABASE_DATABASE;
 export const SUPABASE_PORT = Number(process.env.SUPABASE_PORT);
 export const BASE_URL_FE = process.env.BASE_URL_FE;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET_FORGOT_PASSWORD =
+  process.env.JWT_SECRET_FORGOT_PASSWORD;
+export const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
+export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
