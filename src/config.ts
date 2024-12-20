cda66d28bb255f7d32f5837c993f300e5f5433bc
calvin-key <calvin.delanop7@gmail.com>
2024-12-20 10:52:03 +0700

    feat: get events, create event

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,6 @@ export const SUPABASE_USER = process.env.SUPABASE_USER;
 export const SUPABASE_PASSWORD = process.env.SUPABASE_PASSWORD;
 export const SUPABASE_DATABASE = process.env.SUPABASE_DATABASE;
 export const SUPABASE_PORT = Number(process.env.SUPABASE_PORT);
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
